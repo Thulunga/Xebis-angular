22a06c84677cfe86d3362748b1c855013a5b170d
Thulunga Basumatary <thulunga.basumatary@soti.net>
2022-08-16 12:41:07 +0530

    testing

---
+++ src/app/test.component.ts
@@ -0,0 +1,11 @@
+import {Component } from '@angular/core';
+
+@Component({
+    selector: 'app-test',
+    template: '<h1>Test Component</h1>',
+    styles: []
+})
+
+export class TestComponent{
+
+}
